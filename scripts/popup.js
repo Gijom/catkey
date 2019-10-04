e7c4dbfc4fda42d4da901b5404e32d86dc4f7910
Guillaume Chanel <Guillaume.Chanel@unige.ch>
2019-10-04 22:12:07 +0200

    Test modification of header (failed)

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,11 +2,10 @@ el = document.getElementById("important");
 el.addEventListener("click", set_catkey);
 
 async function set_catkey() {
+    console.log(browser);
     console.log('Got for it my catkey !');
     let tagList = await browser.messages.listTags();
-    for (let tag of tagList) {
-        console.log(tag.tag);
-    }
+    console.log(tagList);
 
     // Get selected messages
     let messageList = await browser.mailTabs.getSelectedMessages();
@@ -20,6 +19,16 @@ async function set_catkey() {
 
     let message = messageList.messages[0];
     console.log(message.subject);
+    
+    let full_message = await browser.messages.getFull(message.id);
+    console.log(full_message.headers);
+    full_message.headers.keywords = [tagList[0].tag];
+    ['from'].push('guillaume.chanel@unige.ch')
+    console.log(full_message.headers);
+    console.log(full_message.headers['from']);
+    console.log(full_message.headers['keywords']);
+    browser.messages.archive([message.id]);
+
 
     browser.messages.update(message.id, {tags: [tagList[0].key]});
 }
